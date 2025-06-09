e9b312c8900dd366bba787f98dbeca50483ee79d
Doolotbek-Zaparbekov <victusdz2008@gmail.com>
2025-06-09 21:38:51 +0600

    HW 2

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Напишите название книги')
+    image = models.ImageField(upload_to='books/', verbose_name='Загрузите картинку')
+    description = models.TextField(verbose_name='Дайте описание')
+    TYPE_BOOK =(
+        ('Magic realism', 'Magic realism'),
+        ('Novel', 'Novel'),
+        ('Fantasy', 'Fantasy')
+    )
+    type_book = models.CharField(max_length=100, choices=TYPE_BOOK)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = 'книгу'
+        verbose_name_plural = 'книги'
+    
--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('news_book/', views.book_list_view, name='news_book'),
+  path('news_book/<int:id>/', views.book_detail_view, name='news_book_detail'),
   path('books/', views.books, name='books'),
 ]
