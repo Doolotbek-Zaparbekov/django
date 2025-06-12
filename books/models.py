56370c6deab2fc071a6c4172f9dc8c7ae3889aa3
Doolotbek-Zaparbekov <victusdz2008@gmail.com>
2025-06-12 21:18:33 +0600

    HW 3

--- books/models.py
+++ books/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name='Напишите название книги')
@@ -10,6 +12,7 @@ class Book(models.Model):
         ('Fantasy', 'Fantasy')
     )
     type_book = models.CharField(max_length=100, choices=TYPE_BOOK)
+    link_youtube = models.URLField(verbose_name='вставте ссылку', null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -19,3 +22,37 @@ class Book(models.Model):
         verbose_name = 'книгу'
         verbose_name_plural = 'книги'
     
+class Reviews(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews', null=True, verbose_name='Книга')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор', null=True)
+    text = models.TextField(verbose_name='Напишите отзыв')
+    mark = models.IntegerField(
+        validators=[
+            MinValueValidator(1, message="Оценка должна быть не меньше 1."),
+            MaxValueValidator(5, message="Оценка должна быть не больше 5.")
+        ],
+        verbose_name='Оценка от 1 до 5',
+        default=1
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.book} - {self.author} - {self.mark}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
+
+
+class NewsBook(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name ='Автор'
+        verbose_name_plural ='Автор'
--- books/views.py
+++ books/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . import models
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
+from .models import NewsBook
+from django.db.models import Avg 
+
+
 
 def book_list_view(request):
   if request.method == 'GET':
@@ -10,14 +16,29 @@ def book_list_view(request):
     }
     return render(request, template_name='book.html', context=context  )
   
-  def book_detail_view(request, id):
-    if request.method == 'GET':
-      book_id = get_object_or_404(models.Book, id=id)
-      context = {
-        'book_id': book_id,
-      }
-      return render(request, template_name='book_detail.html', context=context)
+def book_detail_view(request, id):
+  if request.method == 'GET':
+    book_id = get_object_or_404(models.Book, id=id)
+    average_rating = book_id.reviews.aggregate(avg_mark=Avg('mark'))['avg_mark']
 
+    context = {
+        'book_id': book_id,
+        'average_rating': average_rating 
+    }
+    return render(request, template_name='book_detail.html', context=context)
+  
+class NewsBookCreateView(LoginRequiredMixin, CreateView):
+    model = NewsBook
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+  
+def your_view_function(request, book_id):
+    return HttpResponse(f"Вы запросили аудиозапись для книги с ID: {book_id}")
+       
 def books(request):
   if request.method == 'GET':
     return HttpResponse('«Я лежал, кутаясь в свой краешек одеяла, и мне было хорошо. Я стал частью чего-то большого, многоногого и многорукого, теплого и болтливого. Я стал хвостом или рукой, а может быть даже костью. При каждом движении кружилась голова, и все равно давно уже мне не было так уютно.» Дом в котором...')
